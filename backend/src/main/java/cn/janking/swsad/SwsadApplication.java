d5700099ee430fe8e382fc876f37e4f28e4aef87
Janking <1634363254@qq.com>
2019-03-21 00:46:32 +0800

    helloworld阶段
    使用IDEA初始化Spring Boot项目
    http://localhost:8080/hello

---
+++ backend/src/main/java/cn/janking/swsad/SwsadApplication.java
@@ -0,0 +1,13 @@
+package cn.janking.swsad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwsadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwsadApplication.class, args);
+    }
+
+}
---
+++ backend/src/main/java/cn/janking/swsad/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.janking.swsad.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+
+    @RequestMapping("/hello")
+    private String index(){
+        return "Hello World!";
+    }
+}
