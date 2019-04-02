1c12ea7dab50b20b77a0475cfb747c33ded574de
yezo <592601839@qq.com>
2019-04-02 17:27:42 +0800

    update main page
    #未完成
    加了布局模板，修改了文字，加入跳转逻辑函数。

--- frontend/TimeIsMoney/src/views/main.vue
+++ frontend/TimeIsMoney/src/views/main.vue
@@ -1,197 +1,116 @@
-<template>
-  <div class="hello">
-    <div class="container">
-  <div class="dot dot-1"></div>
-  <div class="dot dot-2"></div>
-  <div class="dot dot-3"></div>
-</div>
-
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-  <defs>
-    <filter id="goo">
-      <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7"/>
-    </filter>
-  </defs>
-</svg>
-    <h1>{{ msg }}</h1>
-    <br/>
-    <RadioGroup v-model="bigKinds">
-      <Radio label="film">
-        <Icon type="ios-film"></Icon>
-        <span>Films</span>
-      </Radio>
-      <Radio label="person">
-        <Icon type="ios-person"></Icon>
-        <span>Person</span>
-      </Radio>
-      <Radio label="planet">
-        <Icon type="ios-planet"></Icon>
-        <span>Planets</span>
-      </Radio>
-      <Radio label="species">
-        <Icon type="md-outlet"/>
-        <span>Species</span>
-      </Radio>
-      <Radio label="starship">
-        <Icon type="md-jet"></Icon>
-        <span>Starships</span>
-      </Radio>
-      <Radio label="vehicle">
-        <Icon type="md-pie"/>
-        <span>Vehicle</span>
-      </Radio>
-
-    </RadioGroup>
-    <Input search enter-button="Search" @on-search="handleSearch" style="width: 60%;margin:10px auto"
-           placeholder="Enter a number"/>
-
-    <br/>
-    <Row style="margin-top: 20px;font-size: 14px">
-      <Col span="6" offset="3">
-        <h4>What is this?</h4>
-        <p>The Star Wars API, or "swapi" (Swah-pee) is the world's first quantified and programmatically-accessible data
-          source for all the data from the Star Wars canon universe!</p>
-
-        <p>We've taken all the rich contextual stuff from the universe and formatted into something easier to consume with
-          software. Then we went and stuck an API on the front so you can access it all!</p>
-      </Col>
-      <Col span="6">
-        <h4>How can I use it?</h4>
-        <p>All the data is accessible through our HTTP web API. Consult our
-          <a href="http://127.0.0.1:8080/#/about">documentation</a> if you'd like to get started.</p>
-
-        <p>Helper libraries for popular programming languages are also provided so you can consume swapi in your favourite
-          programming language, in a style that suits you.</p>
-      </Col>
-      <Col span="6">
-        <h4>How can I support this?</h4>
-        <p>With small donations we can keep swapi running for free, please consider throwing us some beer money to say
-          thank you. With every $10 we can keep the site up for another month!</p>
-        <a href="http://127.0.0.1:8080/#/show">about us</a>
-
-        <p>This project is open source and you can contribute on <a href="https://github.com/DalaoBringMeFly">Github</a>.</p>
-      </Col>
-    </Row>
-
-
-  </div>
-</template>
-
-<script>
-
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
-
-  .fade-enter-active, .fade-leave-active, {
-    transition: opacity .5s;
-  }
-
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
-  {
-    opacity: 0;
-  }
-
-  h4 {
-    font-size: 18px;
-  }
-
-
-
-.container {
-  width: 200px;
-  height: 200px;
-  position: absolute;
-  top: 13%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  margin: auto;
-  filter: url('#goo');
-  animation: rotate-move 2s ease-in-out infinite;
+    .layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    /*position: relative;*/
+    border-radius: 4px;
+    overflow: hidden;
+}
+.layout-logo{
+    width: 100px;
+    height: 35px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 10px;
+    text-align: center;
+    line-height: 35px;
+}
+.layout-nav{
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
+}
+#logoN{
+  font-size:20px;
+  color:white;
 }
 
-.dot {
-  width: 70px;
-  height: 70px;
-  border-radius: 50%;
-  background-color: #000;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
+#signB{
+  float:right;
+  font-size:20px;
 }
 
-.dot-3 {
-  background-color: #f74d75;
-  animation: dot-3-move 2s ease infinite, index 6s ease infinite;
-}
 
-.dot-2 {
-  background-color: #10beae;
-  animation: dot-2-move 2s ease infinite, index 6s -4s ease infinite;
-}
-
-.dot-1 {
-  background-color: #ffe386;
-  animation: dot-1-move 2s ease infinite, index 6s -2s ease infinite;
-}
-
-@keyframes dot-3-move {
-  20% {transform: scale(1)}
-  45% {transform: translateY(-18px) scale(.45)}
-  60% {transform: translateY(-90px) scale(.45)}
-  80% {transform: translateY(-90px) scale(.45)}
-  100% {transform: translateY(0px) scale(1)}
-}
-
-@keyframes dot-2-move {
-  20% {transform: scale(1)}
-  45% {transform: translate(-16px, 12px) scale(.45)}
-  60% {transform: translate(-80px, 60px) scale(.45)}
-  80% {transform: translate(-80px, 60px) scale(.45)}
-  100% {transform: translateY(0px) scale(1)}
-}
-
-@keyframes dot-1-move {
-  20% {transform: scale(1)}
-  45% {transform: translate(16px, 12px) scale(.45)}
-  60% {transform: translate(80px, 60px) scale(.45)}
-  80% {transform: translate(80px, 60px) scale(.45)}
-  100% {transform: translateY(0px) scale(1)}
-}
-
-@keyframes rotate-move {
-  55% {transform: translate(-50%, -50%) rotate(0deg)}
-  80% {transform: translate(-50%, -50%) rotate(360deg)}
-  100% {transform: translate(-50%, -50%) rotate(360deg)}
-}
-
-@keyframes index {
-  0%, 100% {z-index: 3}
-  33.3% {z-index: 2}
-  66.6% {z-index: 1}
-}
 
 
 </style>
+<template>
+    <div class="layout">
+        <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                    <div class="layout-logo">
+                        <p id="logoN">M.I.T </p>
+                    </div>
+                    <div class="layout-nav">
+                        <div id="signB">
+                            <MenuItem name="signIn" @click.native="alertsome">
+                            <Icon type="ios-navigate"></Icon>
+                            登陆
+                            </MenuItem>
+                            <MenuItem name="signUp" @click.native="alertsome">
+                            <Icon type="ios-keypad"></Icon>
+                            注册
+                            </MenuItem>
+                        </div>
+                    </div>
+                </Menu>
+            </Header>
+            <Layout>
+                <Sider hide-trigger :style="{background: '#fff'}">
+                    <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+                        <Submenu name="1">
+                            <template slot="title">
+                                <Icon type="ios-navigate"></Icon>
+                                问卷服务
+                            </template>
+                            <MenuItem name="1-1">查看所有问卷</MenuItem>
+                            <MenuItem name="1-2">发布问卷</MenuItem>
+                            <MenuItem name="1-3">我的问卷</MenuItem>
+                            <MenuItem name="1-4">问卷详情</MenuItem>
+                        </Submenu>
+                        <Submenu name="2">
+                            <template slot="title">
+                                <Icon type="ios-keypad"></Icon>
+                                跑腿服务
+                            </template>
+                            <MenuItem name="2-1">拿快递</MenuItem>
+                            <MenuItem name="2-2">拿外卖</MenuItem>
+                        </Submenu>
+                        <Submenu name="3">
+                            <template slot="title">
+                                <Icon type="ios-analytics"></Icon>
+                                约馆助手
+                            </template>
+                            <MenuItem name="3-1">我要约馆</MenuItem>
+                            <MenuItem name="3-2">我要约球</MenuItem>
+                        </Submenu>
+                    </Menu>
+                </Sider>
+                <Layout :style="{padding: '0 24px 24px'}">
+                    <Breadcrumb :style="{margin: '24px 0'}">
+                        <BreadcrumbItem>Home</BreadcrumbItem>
+                        <BreadcrumbItem>Components</BreadcrumbItem>
+                        <BreadcrumbItem>Layout</BreadcrumbItem>
+                    </Breadcrumb>
+                    <Content :style="{padding: '24px', minHeight: '500px', background: '#fff'}">
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        </Layout>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        alertsome() {
+            alert("yes");
+        }
+    }
+
+}
+</script>
