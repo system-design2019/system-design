ea8b8fc0bb11664eab59a93eadd0f68f3cb17085
yezo <592601839@qq.com>
2019-04-02 14:58:28 +0800

    update whole router
    easy router is ok，did not include the in-page route

--- frontend/TimeIsMoney/src/router/router.js
+++ frontend/TimeIsMoney/src/router/router.js
@@ -2,26 +2,94 @@
 
 const routers = [
     {
-        path: '/',
+        path: '/', //跳转页面
         meta: {
-            title: 'Begin'
+            title: 'T.I.M'
         },
-        component: (resolve) => require(['../views/begin.vue'], resolve)
+        component: (resolve) => require(['../views/jump.vue'], resolve)
     },
     {
-        path: '/sign',
+        path: '/sign',  //登陆注册页面，登陆注册模块使用动态组件切换，参考知乎
         meta: {
-            title: 'signIn'
+            title: 'Sign'
         },
-        component: (resolve) => require(['../views/index.vue'], resolve)
+        component: (resolve) => require(['../views/sign.vue'], resolve)
     },
     {
-        path: '/index',
+        path: '/findpass',  //找回密码页面
         meta: {
-            title: 'index'
+            title: 'FindPass'
         },
-        component: (resolve) => require(['../views/hello.vue'], resolve)
+        component: (resolve) => require(['../views/findpass.vue'], resolve)
+    },    
+    {
+        path: '/main',  //主页（分游客 & 登陆用户）
+        meta: {
+            title: 'Main'
+        },
+        component: (resolve) => require(['../views/main.vue'], resolve)
+    },
+    {
+        path:'/questionShow', //问卷展示页面
+        meta:{
+            title:"QuestionMain"
+        },
+        component:(resolve) => require(['../views/questionShow.vue'],resolve)
+    },
+    {
+        path:'/questionDesign', //问卷设计页面
+        meta:{
+            title:"QuestionDesign"
+        },
+        component:(resolve) => require(['../views/questionDesign.vue'],resolve)
+    },
+    {
+        path:'/questionDetail', //问卷详情页面
+        meta:{
+            title:"QuestionDetail"
+        },
+        component:(resolve) => require(['../views/questionDetail.vue'],resolve)
+    },
+    {
+        path:'/questionWrite', //问卷填写页面
+        meta:{
+            title:"QuestionWrite"
+        },
+        component:(resolve) => require(['../views/questionWrite.vue'],resolve)
+    },
+    {
+        path:'/personal', //personal page
+        meta:{
+            title:"Personal Info"
+        },
+        component:(resolve) => require(['../views/personal.vue'],resolve)
+    },
+    {
+        path:'/infoChange', //change the personal info
+        meta:{
+            title:"infoChange"
+        },
+        component:(resolve) => require(['../views/infoChange.vue'],resolve)
+    },
+    {
+        path:'/historyCheck', //check the user's history deal
+        meta:{
+            title:"historyCheck"
+        },
+        component:(resolve) => require(['../views/historyCheck.vue'],resolve)
+    },
+    {
+        path:'/receiveBox', //check the user's receiveBox
+        meta:{
+            title:"receiveBox"
+        },
+        component:(resolve) => require(['../views/receiveBox.vue'],resolve)
     }
+
+
+
+
+
 
 ];
 export default routers;
---
+++ frontend/TimeIsMoney/src/views/jump.vue
@@ -0,0 +1,52 @@
+<style scoped lang="less">
+    .index{
+        width: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        text-align: center;
+        h1{
+            height: 150px;
+            img{
+                height: 100%;
+            }
+        }
+        h2{
+            color: #666;
+            margin-bottom: 200px;
+            p{
+                margin: 0 0 50px;
+            }
+        }
+        .ivu-row-flex{
+            height: 100%;
+        }
+    }
+</style>
+<template>
+    <div class="index">
+        <Row type="flex" justify="center" align="middle">
+            <Col span="24">
+                <h1>
+                    <img src="../images/logo.png">
+                </h1>
+                <h2>
+                    <p>Welcome to your iView app!</p>
+                    <Button @click="handleStart">Start iView</Button>
+                </h2>
+            </Col>
+        </Row>
+    </div>
+</template>
+<script>
+    export default {
+        methods: {
+            handleStart () {   
+                this.$router.push({  //跳转到不同后缀的页面，同理可以有多个子后缀，从而实现页面跳转
+                    path:'/main'
+                })
+            }
+        }
+    }
+</script>
