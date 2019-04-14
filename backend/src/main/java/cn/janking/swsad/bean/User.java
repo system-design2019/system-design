2c3c20cf859ae8a7a133b1711ffde1a06f7dde5f
Janking <1634363254@qq.com>
2019-04-15 02:11:21 +0800

    完善用户属性，添加上传头像功能，统一回复JSON格式

--- backend/src/main/java/cn/janking/swsad/bean/User.java
+++ backend/src/main/java/cn/janking/swsad/bean/User.java
@@ -9,36 +9,53 @@ public class User {
     /*用户数量*/
     private static int count = 10000;
     /*登录资料*/
+    //数据库自动生成
     private int id;
     private String password;
+    //邮箱和手机号均唯一，且不为空，初始状态以"$"开头，表示未初始化
     private String email;
     private String phone;
     /*实名资料*/
-/*  private String name;
+    //用户名 唯一
+    private String name;
     private String studentId;
+    //初始化-1
+    //本科1 2 3 4 5
+    //硕士11 22 33
+    //博士111 222 333
     private int grade;
     private String major;
+    //初始化-1
+    //女0，男1
     private int gender;
+    //初始化-1
+    //范围0~150
     private int age;
-    *//*个性资料*//*
+    /*个性资料*/
     private String nickname;
+    //保存URL
+    /*
     private String face;
     private String weChatPay;
     private String aliPay;
     private String QQ;
-    private int credit;*/
+    private int credit;
+    */
     static public void initCount(int c){
         count = c + 10000;
     }
 
     public User(){
         password = "default_password";
-    }
-    public void modifyUser(){
-        if(email == null)
-            email = "$"+count;
-        if(phone == null)
-            phone = "$"+count;
+        email = "$"+count;
+        phone = "$"+count;
+        name = "$"+count;
+        studentId = null;
+        grade = -1;
+        major = null;
+        gender = -1;
+        age = -1;
+        nickname = null;
     }
     public void setId(int id) {
         this.id = id;
@@ -49,11 +66,56 @@ public class User {
     }
 
     public void setEmail(String email) {
-        this.email = email;
+
+        if(email == null || email.isEmpty())
+            this.email = "$"+count;
+        else
+            this.email = email;
     }
 
     public void setPhone(String phone) {
-        this.phone = phone;
+        if(phone == null || phone.isEmpty())
+            this.phone = "$"+count;
+        else
+            this.phone = phone;
+    }
+
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public static void setCount(int count) {
+        User.count = count;
+    }
+
+    public void setName(String name) {
+
+        if(name == null || name.isEmpty())
+            this.name = "$"+count;
+        else
+            this.name = name;
     }
 
     public int getId() {
@@ -71,4 +133,37 @@ public class User {
     public String getPhone() {
         return phone;
     }
+    public String getStudentId() {
+        return studentId;
+    }
+
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
--- backend/src/main/java/cn/janking/swsad/controller/Controller.java
+++ backend/src/main/java/cn/janking/swsad/controller/Controller.java
@@ -1,17 +1,29 @@
 package cn.janking.swsad.controller;
 
+import cn.janking.swsad.bean.Message;
 import cn.janking.swsad.bean.User;
 import cn.janking.swsad.mapper.UserMapper;
 import cn.janking.swsad.singleton.SingletonMybatis;
 import com.fasterxml.jackson.core.JsonFactory;
+import com.sun.xml.internal.ws.api.model.MEP;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.tomcat.util.bcel.Const;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.json.JsonSimpleJsonParser;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.jws.soap.SOAPBinding;
+import javax.servlet.ServletContext;
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 @RestController
 @EnableAutoConfiguration
@@ -23,8 +35,6 @@ public class Controller {
         /*用户数量*/
         User.initCount(sqlSessionFactory.openSession(true).getMapper(UserMapper.class).getCount());
     }
-
-
 
     //如果方法上的RequestMapping没有value，则此方法默认被父路径调用
     //默认回调
@@ -61,7 +71,8 @@ public class Controller {
     }
     /*获取所有用户*/
     @RequestMapping(method = RequestMethod.GET,value = "/users")
-    public List<User> getUsers(){
+    public Message<List<User>> getUsers(){
+        Message<List<User>> message = new Message<>();
         List<User> listUsers;
         //获取一个连接
         SqlSession sqlSession = sqlSessionFactory.openSession();
@@ -70,103 +81,187 @@ public class Controller {
             UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
             //调用接口中的方法去执行xml文件中的SQL语句
             listUsers = userMapper.getUsers();
+            message.setData(listUsers);
+            message.setSuccess(true);
+            message.setMsg("获取成功");
             //要提交后才会生效
             sqlSession.commit();
-        }finally {
+        }catch (Exception e){
+            message.setData(null);
+            message.setSuccess(false);
+            message.setMsg("获取失败:" + e.getMessage());
+        }
+        finally {
             //最后记得关闭连接
             sqlSession.close();
         }
 
-        return listUsers;
+        return message;
     }
     /*注册：插入用户数据
      * 成功返回true
      * 用户名存在返回false*/
     @RequestMapping(method = RequestMethod.POST,value = "/register")
-    public boolean register(@RequestBody User user){
+    public Message<String> register(@RequestBody User user){
+        Message<String> message = new Message<>();
+        System.out.println(user);
         SqlSession sqlSession = sqlSessionFactory.openSession();
         try {
             UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-            user.modifyUser();
             userMapper.insert(user);
+            message.setSuccess(true);
+            message.setMsg("注册成功");
             sqlSession.commit();
         }catch (Exception e){
             e.printStackTrace();
-            return false;
+            message.setSuccess(false);
+            message.setMsg("注册失败:" + e.getMessage());
+            return message;
         }finally {
             sqlSession.close();
         }
-        return true;
+        return message;
     }
     /*登录：判断用户名和密码是否一致
      * 一致返回当前用户的信息
      * 不一致返回null*/
     @RequestMapping(method = RequestMethod.POST,value = "/user")
-    public User login(@RequestBody User loginUser){
+    public Message<User> login(@RequestBody User loginUser){
+        Message<User> message = new Message<>();
         User user = null;
         SqlSession sqlSession = sqlSessionFactory.openSession();
         //判断是手机还是邮箱
         System.out.println(loginUser.getPhone() + loginUser.getEmail());
-        if(loginUser.getPhone() != null && !loginUser.getPhone().isEmpty()){
+        if(loginUser.getPhone() != null && !loginUser.getPhone().isEmpty() && loginUser.getPhone().charAt(0) != '$'){
             try {
                 UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
                 user = userMapper.getByPhone(loginUser.getPhone());
                 sqlSession.commit();
             }catch (Exception e){
                 e.printStackTrace();
+                message.setMsg("登录失败:" + e.getMessage());
+                return message;
             }finally {
                 sqlSession.close();
             }
         }
-        if(loginUser.getEmail() != null && !loginUser.getEmail().isEmpty()){
+        else if(loginUser.getEmail() != null && !loginUser.getEmail().isEmpty() && loginUser.getEmail().charAt(0) != '$'){
             try {
                 UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
                 user = userMapper.getByEmail(loginUser.getEmail());
                 sqlSession.commit();
             }catch (Exception e){
                 e.printStackTrace();
+                message.setMsg("登录失败:" + e.getMessage());
+                return message;
             }finally {
                 sqlSession.close();
             }
         }
         //判断密码是否正确
-        if(user!=null && user.getPassword().equals(loginUser.getPassword()))
-            return user;
-        return null;
+        if(user!=null && user.getPassword().equals(loginUser.getPassword())){
+            message.setMsg("登录成功");
+            message.setSuccess(true);
+            message.setData(user);
+            return message;
+        }
+        message.setMsg("登录失败：用户名或密码错误");
+        message.setSuccess(false);
+        message.setData(null);
+        return message;
     }
     /*更新用户数据*/
     @RequestMapping(method = RequestMethod.PUT,value = "/user")
-    public boolean updateUser(@RequestBody User user){
+    public Message<String> updateUser(@RequestBody User user){
+        Message<String> message = new Message<>();
         SqlSession sqlSession = sqlSessionFactory.openSession();
         boolean result;
         try {
             UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
             result = userMapper.updateUser(user);
-            sqlSession.commit();
-        }finally {
-            sqlSession.close();
-        }
-        return result;
-    }
-
-    /*删除指定用户*/
-    @RequestMapping(method = RequestMethod.DELETE,value = "/user/{userId}")
-    public boolean deleteUser(@PathVariable int userId){
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        boolean result;
-        try {
-            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-            result = (userMapper.deleteUser(userId) > 0);
-            System.out.println(result);
+            message.setSuccess(result);
+            message.setMsg("修改成功");
             sqlSession.commit();
         }catch (Exception e){
             e.printStackTrace();
-            return false;
+            message.setMsg("修改失败:" + e.getMessage());
+            message.setSuccess(false);
         }
         finally {
             sqlSession.close();
         }
-        return result;
+        return message;
+    }
+
+    /*上传用户头像,返回图片URL*/
+    @RequestMapping("/upload")
+    @ResponseBody
+    public Message handleFileUpload(@RequestParam("file") MultipartFile file) {
+        Message<String> message = new Message<>();
+        if (!file.isEmpty()) {
+            try {
+                //获取跟目录
+                File path = new File(ResourceUtils.getURL("classpath:").getPath());
+                String fileName = path.getAbsolutePath() + "/static/faces/" + file.getOriginalFilename();
+                if(!path.exists())
+                    path = new File("");
+                BufferedOutputStream out = new BufferedOutputStream(
+                        new FileOutputStream(new File(fileName)));
+                System.out.println(file.getName());
+                message.setSuccess(true);
+                message.setMsg("上传成功");
+                message.setData(fileName);
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                message.setSuccess(false);
+                message.setMsg("上传失败," + e.getMessage());
+                return message;
+            } catch (IOException e) {
+                e.printStackTrace();
+                message.setSuccess(false);
+                message.setMsg("上传失败," + e.getMessage());
+                return message;
+            }
+
+            return message;
+
+        } else {
+            message.setSuccess(false);
+            message.setMsg("上传失败,文件为空！" );
+            return message;
+        }
+    }
+    /*删除指定用户
+    * 通过id查找
+    * 不用验证密码！！*/
+    @RequestMapping(method = RequestMethod.DELETE,value = "/user/{userId}")
+    public Message<String> deleteUser(@PathVariable int userId){
+        Message<String> message = new Message<>();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        int result;
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            result = userMapper.deleteUser(userId);
+            if(result > 0){
+                message.setSuccess(true);
+                message.setMsg("删除成功");
+            }else{
+                message.setSuccess(false);
+                message.setMsg("不存在该用户");
+            }
+            sqlSession.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            message.setMsg("删除失败:"+e.getMessage());
+            return message;
+        }
+        finally {
+            sqlSession.close();
+        }
+        return message;
     }
 
 
