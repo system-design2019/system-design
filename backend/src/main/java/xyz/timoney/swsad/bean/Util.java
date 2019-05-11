141cbd3116765f6e2ba6a0f25155a246cfa2b49f
Janking <1634363254@qq.com>
2019-05-12 03:35:04 +0800

    更改项目所有者为timoney.xyz

---
+++ backend/src/main/java/xyz/timoney/swsad/bean/Util.java
@@ -0,0 +1,61 @@
+package xyz.timoney.swsad.bean;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.util.UUID;
+
+public class Util {
+    /*文件传输*/
+    public static ResponseEntity<FileSystemResource> export(File file) {
+        if (file == null) {
+            return null;
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", "attachment; filename=" + System.currentTimeMillis() + ".doc");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("Last-Modified", new Date().toString());
+        headers.add("ETag", String.valueOf(System.currentTimeMillis()));
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new FileSystemResource(file));
+    }
+    /**
+     * 生成随机文件名：当前年月日时分秒+五位随机数
+     *
+     * @return
+     */
+    public static String getRandomFileName() {
+
+        SimpleDateFormat simpleDateFormat;
+
+        simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+
+        Date date = new Date();
+
+        String str = simpleDateFormat.format(date);
+
+        Random random = new Random();
+
+        int rannum = (int) (random.nextDouble() * (99999 - 10000 + 1)) + 10000;// 获取5位随机数
+
+        return rannum + str;// 当前时间
+    }
+
+    public static String getUUID(){
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+    }
+}
---
+++ backend/src/main/java/xyz/timoney/swsad/mapper/QuestionnaireMapper.java
@@ -0,0 +1,17 @@
+package xyz.timoney.swsad.mapper;
+import java.util.List;
+import xyz.timoney.swsad.bean.questionnaire;
+public interface QuestionnaireMapper {
+    /*初始化表*/
+    void questionnaireTableInit();
+    /*添加一个问卷*/
+    boolean insert(questionnaire ques);
+    /*获取所有问卷*/
+    List<questionnaire> getAllQues();
+
+    /*获取问卷详情*/
+    questionnaire getQuesByID(int quesID);
+    /*获取问卷内容*/
+    String getQuesCont(int quesID);
+    int getCount();
+}
