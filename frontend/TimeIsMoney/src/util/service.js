4b1a24a745097002e91d13c51e0c8e907a9a18e5
yezo <592601839@qq.com>
2019-05-22 19:59:17 +0800

    Revert "change url"
    This reverts commit 90bb7cc78c9ba36bf82b97959eb8e9c3cfb6cfa4.

--- frontend/TimeIsMoney/src/util/service.js
+++ frontend/TimeIsMoney/src/util/service.js
@@ -2,45 +2,45 @@ import axios from 'axios'
 
 // 创建axios实例
 const service = axios.create({
-    baseURL: 'http://api.timoney.xyz', // node环境的不同，对应不同的baseURL
-    timeout: 5000, // 请求的超时时间
-    method: 'get',
-    headers: {
-        "Content-Type": "application/json;charset=UTF-8"
-    },
-    withCredentials: true // 允许携带cookie
+  baseURL: 'http://172.26.70.9:8080', // node环境的不同，对应不同的baseURL
+  timeout: 5000, // 请求的超时时间
+  method: 'get',
+  headers: {  
+    "Content-Type": "application/json;charset=UTF-8"
+  },
+  withCredentials: true // 允许携带cookie
 })
 
 // 发送请求前处理request的数据
-axios.defaults.transformRequest = [function(data) {
-    let newData = ''
-    for (let k in data) {
-        newData += encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) + '&'
-    }
-    return newData
+axios.defaults.transformRequest = [function (data) {
+  let newData = ''
+  for (let k in data) {
+    newData += encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) + '&'
+  }
+  return newData
 }]
 
 // request拦截器
 service.interceptors.request.use(
-    config => {
-        // 发送请求之前，要做的业务
-        return config
-    },
-    error => {
-        // 错误处理代码
-        return Promise.reject(error)
-    }
+  config => {
+    // 发送请求之前，要做的业务
+    return config
+  },
+  error => {
+    // 错误处理代码
+    return Promise.reject(error)
+  }
 )
 
 // response拦截器
 service.interceptors.response.use(
-    response => {
-        // 数据响应之后，要做的业务
-        return response
-    },
-    error => {
-        return Promise.reject(error)
-    }
+  response => {
+    // 数据响应之后，要做的业务
+    return response
+  },
+  error => {
+    return Promise.reject(error)
+  }
 )
 
 export default service
