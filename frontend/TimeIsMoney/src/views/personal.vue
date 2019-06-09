02bf22702fcf3ebcc40d52eb684b29ddd8ba1921
yezo <592601839@qq.com>
2019-06-09 23:26:57 +0800

    solve css
    add scroll

--- frontend/TimeIsMoney/src/views/personal.vue
+++ frontend/TimeIsMoney/src/views/personal.vue
@@ -49,6 +49,10 @@
 
     }
 
+    .ivu-tabs-content,.ivu-tabs-content-animated{
+        height:450px;
+    }
+
     Row {
         margin: 30px;
     }
@@ -182,27 +186,39 @@
         <div class="personal" style="margin: 0 10%">
             <Tabs value="credit" style="font-size: 20px">
                 <TabPane label="我发布的" name="credit">
-                    <div id="Dynamic" v-for="(ques,index) in publishLists">
-                        <div>
-                            <span id="dynamicDate" style="font-size:20px;color:red;"> {{ques.infos.startTime}} </span> <span style="font-size:15px;color:gray;">我发布了</span>
-                        </div>
-                        <task :data="ques" :key="index" type="1" mode="0" @click.native="getDetail(ques.quesID)"></task>
+                    <div>
+                        <Scroll height="450">
+                            <div id="Dynamic" v-for="(ques,index) in publishLists">
+                                <div>
+                                    <span id="dynamicDate" style="font-size:20px;color:red;"> {{ques.infos.startTime}} </span> <span style="font-size:15px;color:gray;">我发布了</span>
+                                </div>
+                                <task :data="ques" :key="index" type="1" mode="0" @click.native="getDetail(ques.quesID)"></task>
+                            </div>
+                        </Scroll>
                     </div>
                 </TabPane>
                 <TabPane label="我参与的" name="history">
-                    <div id="Dynamic" v-for="(ques,index) in attendLists">
-                        <div>
-                            <span id="dynamicDate" style="font-size:20px;color:red;"> {{ques.infos.startTime}} </span><span style="font-size:15px;color:gray;">我参与了</span>
-                        </div>
-                        <task :data="ques" :key="index" type="1" mode="1" @click.native="getDetail(ques.quesID)"></task>
+                    <div>
+                        <Scroll height="450">
+                            <div id="Dynamic" v-for="(ques,index) in attendLists">
+                                <div>
+                                    <span id="dynamicDate" style="font-size:20px;color:red;"> {{ques.infos.startTime}} </span><span style="font-size:15px;color:gray;">我参与了</span>
+                                </div>
+                                <task :data="ques" :key="index" type="1" mode="1" @click.native="getDetail(ques.quesID)"></task>
+                            </div>
+                        </Scroll>
                     </div>
                 </TabPane>
                 <TabPane label="我的收藏" name="collect">
-                    <div id="Dynamic" v-for="(ques,index) in collectLists">
-                        <div>
-                            <span id="dynamicDate" style="font-size:20px;color:red;"> {{ques.infos.startTime}} </span><span style="font-size:15px;color:gray;">我收藏了</span>
-                        </div>
-                        <task :data="ques" :key="index" type="1" mode="1" @click.native="getDetail(ques.quesID)"></task>
+                    <div>
+                        <Scroll height="450">
+                            <div id="Dynamic" v-for="(ques,index) in collectLists">
+                                <div>
+                                    <span id="dynamicDate" style="font-size:20px;color:red;"> {{ques.infos.startTime}} </span><span style="font-size:15px;color:gray;">我收藏了</span>
+                                </div>
+                                <task :data="ques" :key="index" type="1" mode="1" @click.native="getDetail(ques.quesID)"></task>
+                            </div>
+                        </Scroll>
                     </div>
                 </TabPane>
             </Tabs>
