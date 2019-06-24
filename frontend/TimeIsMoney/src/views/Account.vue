34c6a385737e68b448403e6228436c6548714153
yezo <592601839@qq.com>
2019-06-25 00:25:25 +0800

    test buttons

--- frontend/TimeIsMoney/src/views/Account.vue
+++ frontend/TimeIsMoney/src/views/Account.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        
+    <div class="myAccount">
+        <div style="margin-top:40px;">
+            <input />
+            <Button @click.native="chong">充值</Button>
+            <Button @click.native="hua">提现</Button>
+        </div>
     </div>
 </template>
 <script>
@@ -12,9 +16,23 @@ import detail from "./components/Detail.vue"
 import service from './../util/service.js'
 
 export default {
+    data() {
+        return {
+            moneycount: 0
+        }
+    },
+    methods: {
+        chong() {
+            alert("冲他一个亿！");
+        },
+        hua() {
+            alert("提他一个亿！");
+        }
+    },
+    mounted() {
 
+    }
 }
 </script>
 <style>
-
 </style>
