1fe00b8e25f20a748d00165a9ec01fb8bb861185
yezo <592601839@qq.com>
2019-06-25 15:56:33 +0800

    finish get all assets

--- frontend/TimeIsMoney/src/views/Account.vue
+++ frontend/TimeIsMoney/src/views/Account.vue
@@ -2,8 +2,12 @@
     <div class="myAccount">
         <div style="margin-top:40px;">
             <input v-model="moneycount" type="text" style="margin-left:30px" />
-            <Button @click.native="chong">充值</Button>
-            <Button @click.native="hua">提现</Button>
+            <Button @click.native="rechargeAsset">充值</Button>
+            <Button @click.native="withdrawAsset">提现</Button>
+            <Button @click.native="getLog">获取交易记录</Button>
+        </div>
+        <div id="The log">
+            <span>{{logs}}</span>
         </div>
     </div>
 </template>
@@ -15,19 +19,26 @@ import service from './../util/service.js'
 export default {
     data() {
         return {
-            moneycount: 0
+            moneycount: 0,
+
         }
     },
     computed: mapState('Personal', {
         personDetail: 'personalInfo',
+        logs: 'allDeals'
     }),
     methods: {
-        chong() {
+        rechargeAsset() {
             alert("冲他这么多： " + this.moneycount);
         },
-        hua() {
+        withdrawAsset() {
             alert("我用户【" + this.personDetail.id + "】充他个一个亿！");
+        },
+        getLog() {
+            this.$store.dispatch('Personal/GET_ASSET');
+            alert("获取交易记录");
         }
+
     },
     mounted() {
         this.$store.dispatch('Personal/GET_INFO'); //分发action
